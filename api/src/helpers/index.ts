1c855676d6f4b1d946ae0054f53a6793542fefe1
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-03-21 15:55:17 +0000

    Remove unused helper

--- api/src/helpers/index.ts
+++ api/src/helpers/index.ts
@@ -5,10 +5,6 @@ export function isNotUndefined<T>(val: T | undefined): val is T {
   return typeof val !== "undefined";
 }
 
-export function isUndefined<T>(val: T | undefined): val is T {
-  return typeof val === "undefined";
-}
-
 export function isString(v: any): v is string {
   return typeof v === "string";
 }
