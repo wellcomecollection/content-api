425f905885c11e9a6f1c6c9a1f1ef9bd38f74f7c
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-03-23 15:44:30 +0000

    move types

--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -1,46 +1,47 @@
-import * as prismic from "@prismicio/client";
-import { Client as ElasticClient } from "@elastic/elasticsearch";
+// TODO keep?
+import { ArticleFormatId, PrismicImage } from "../../../pipeline/src/types";
 
-// Generic types
-export type ImageDimensions = {
-  width?: number;
-  height?: number;
+// Main article type
+export type Article = {
+  type: "Article";
+  id: string;
+  title: string;
+  publicationDate: string;
+  contributors: Contributor[];
+  format?: ArticleFormat;
+  image?: Image;
+  caption?: string;
 };
 
-// From Prismic data
-import { ArticlePrismicDocument } from "./prismic/articles";
-export type { ArticlePrismicDocument };
-
-import {
-  DataInterface,
-  InferDataInterface,
-  CommonPrismicFields,
-} from "./prismic";
-export type { DataInterface, InferDataInterface, CommonPrismicFields };
-
-import { WithContributors } from "./prismic/contributors";
-export type { WithContributors };
-
-import {
-  ArticleFormatId,
-  PrismicArticleFormat,
-  ContentType,
-} from "./prismic/formats";
-export type { ArticleFormatId, PrismicArticleFormat, ContentType };
-
-import { PrismicImage } from "./prismic/images";
-export type { PrismicImage };
-
-// Transformed types
-import { Article, Contributor, ArticleFormat, Image } from "./transformed";
-export type { Article, Contributor, ArticleFormat, Image };
-
-// Generic types
-export type Clients = {
-  prismic: prismic.Client;
-  elastic: ElasticClient;
+// Image
+export type Image = PrismicImage & {
+  type: "PrismicImage";
 };
 
+// Contributors (e.g. author, photographer)
+type BasicContributorInformation = {
+  id: string;
+  label?: string;
+};
+
+export type Contributor = {
+  type: "Contributor";
+  contributor?: BasicContributorInformation & {
+    type: "Person" | "Organisation";
+  };
+  role?: BasicContributorInformation & {
+    type: "EditorialContributorRole";
+  };
+};
+
+// Article formats (e.g. webcomics, podcast, interview)
+export type ArticleFormat = {
+  type: "ArticleFormat";
+  id: ArticleFormatId;
+  label?: string;
+};
+
+// Generic types
 export type Displayable<T = any> = {
   display: T;
 };
---
+++ pipeline/src/types/index.ts
@@ -0,0 +1,42 @@
+import * as prismic from "@prismicio/client";
+import { Client as ElasticClient } from "@elastic/elasticsearch";
+
+// Generic types
+export type ImageDimensions = {
+  width?: number;
+  height?: number;
+};
+
+// From Prismic data
+import { ArticlePrismicDocument } from "./prismic/articles";
+export type { ArticlePrismicDocument };
+
+import {
+  DataInterface,
+  InferDataInterface,
+  CommonPrismicFields,
+} from "./prismic";
+export type { DataInterface, InferDataInterface, CommonPrismicFields };
+
+import { WithContributors } from "./prismic/contributors";
+export type { WithContributors };
+
+import {
+  ArticleFormatId,
+  PrismicArticleFormat,
+  ContentType,
+} from "./prismic/formats";
+export type { ArticleFormatId, PrismicArticleFormat, ContentType };
+
+import { PrismicImage } from "./prismic/images";
+export type { PrismicImage };
+
+// Transformed types
+import { Article, Contributor, ArticleFormat, Image } from "./transformed";
+export type { Article, Contributor, ArticleFormat, Image };
+
+// Generic types
+export type Clients = {
+  prismic: prismic.Client;
+  elastic: ElasticClient;
+};
