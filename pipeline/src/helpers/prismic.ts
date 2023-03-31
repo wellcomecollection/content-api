87e60ab5757d65521c52d6d8a88b4774139ba437
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-03-31 16:25:22 +0100

    Fix strange merge conflict?

--- pipeline/src/helpers/prismic.ts
+++ pipeline/src/helpers/prismic.ts
@@ -56,27 +56,6 @@ const graphQueryArticles = `{
         }
       }
     }
-    webcomics {
-      title
-      format {
-        title
-      }
-      promo
-      contributors {
-        ...contributorsFields
-        role {
-          title
-        }
-        contributor {
-          ... on people {
-            name
-          }
-          ... on organisations {
-            name
-          }
-        }
-      }
-    }
   }`.replace(/\n(\s+)/g, "\n"); // Pre-emptive removal of whitespaces as requests to the Prismic Rest API are limited to 2048 characters.
 
 // TODO Review what prismic function we use here.
