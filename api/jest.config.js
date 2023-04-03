fb992fd6f3cbaaeb38eed16285f2524db1b82418
jamie <j.parkinson@wellcome.ac.uk>
2023-04-03 10:17:52 +0100

    Remove jest path name stuff

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,11 +1,5 @@
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("../tsconfig.base.json");
-
 module.exports = {
   displayName: "API",
   preset: "ts-jest",
   testEnvironment: "node",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: "<rootDir>/..",
-  }),
 };
