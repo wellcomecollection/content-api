56893b8035dc4b47369be95f50023e088ee1f519
jamie <j.parkinson@wellcome.ac.uk>
2023-04-06 14:45:28 +0100

    Deploy the lambda

--- .buildkite/scripts/upload_lambda_package.sh
+++ .buildkite/scripts/upload_lambda_package.sh
@@ -8,7 +8,7 @@ SERVICE_NAME=$2
 ZIP_FILE=$3
 
 S3_BUCKET="wellcomecollection-content-api-packages"
-S3_KEY="${PIPELINE_NAMESPACE}/${SERVICE_NAME}/${BUILDKITE_COMMIT}.zip"
+S3_KEY="${PIPELINE_NAMESPACE}/${SERVICE_NAME}/ref.${BUILDKITE_COMMIT}.zip"
 
 echo "uploading package for $SERVICE_NAME to S3"
 aws s3 cp $ZIP_FILE "s3://${S3_BUCKET}/${S3_KEY}"
