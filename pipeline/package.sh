69e4b860ba919e660b06f13dc31cb274f82d8a5f
jamie <j.parkinson@wellcome.ac.uk>
2023-04-06 14:53:15 +0100

    Put the right things in the package

--- pipeline/package.sh
+++ pipeline/package.sh
@@ -10,7 +10,7 @@ yarn install --frozen-lockfile --production
 pushd dist
 zip -rq ../package.zip .
 popd
-zip -urq package.zip ./node_modules
+zip -urq package.zip ../node_modules
 
 # Restore dev tools
 yarn install --frozen-lockfile
