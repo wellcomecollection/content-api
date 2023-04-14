ee52ade9077cbefc71d31db50b324bf907af9808
jamie <j.parkinson@wellcome.ac.uk>
2023-04-14 16:44:12 +0100

    These are plausibly the correct paths

--- pipeline/package.sh
+++ pipeline/package.sh
@@ -3,14 +3,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+ROOT=$(git rev-parse --show-toplevel)
+PROJECT_DIR="$ROOT/pipeline"
+PACKAGE_FILE="$PROJECT_DIR/package.zip"
+
 yarn clean
 yarn build
 yarn install --frozen-lockfile --production
 
-pushd dist
-zip -rq ../package.zip .
-popd
-zip -urq package.zip ../node_modules
+cd $PROJECT_DIR/dist
+zip -rq $PACKAGE_FILE .
+cd $ROOT
+zip -urq $PACKAGE_FILE node_modules
 
 # Restore dev tools
 yarn install --frozen-lockfile
