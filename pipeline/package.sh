38c2f4bf22cd341234a654a3e1f50488867a2d25
jamie <j.parkinson@wellcome.ac.uk>
2023-04-17 12:28:32 +0100

    Use a proper bundler

--- pipeline/package.sh
+++ pipeline/package.sh
@@ -3,18 +3,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-ROOT=$(git rev-parse --show-toplevel)
-PROJECT_DIR="$ROOT/pipeline"
-PACKAGE_FILE="$PROJECT_DIR/package.zip"
+yarn run esbuild ./src/lambda.js \
+  --bundle \
+  --platform=node \
+  --target=node18 \
+  --sourcemap=external \
+  --outdir=dist
 
-yarn clean
-yarn build
-yarn install --frozen-lockfile --production
-
-cd $PROJECT_DIR/dist
-zip -rq $PACKAGE_FILE .
-cd $ROOT
-zip -urq $PACKAGE_FILE node_modules
-
-# Restore dev tools
-yarn install --frozen-lockfile
+pushd dist
+zip -rq ../package.zip .
