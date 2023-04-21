1fd041804dcbd663837340593bf045c90e1fc4e1
jamie <j.parkinson@wellcome.ac.uk>
2023-04-21 11:39:29 +0100

    Should specify variable correctly

--- infrastructure/pipeline.tf
+++ infrastructure/pipeline.tf
@@ -1,8 +1,8 @@
 module "pipeline" {
   source = "./pipeline_stack"
 
-  pipeline_date   = "2023-03-24"
-  window_duration = "15 minutes"
+  pipeline_date           = "2023-03-24"
+  window_duration_minutes = 15
 
   logging_cluster_id = local.logging_cluster_id
   network_config     = local.network_config
