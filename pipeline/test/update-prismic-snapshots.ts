14ed9d442f380a883cc0c608150a737d0144b457
jamie <j.parkinson@wellcome.ac.uk>
2023-04-28 16:13:15 +0100

    Self-documenting script

--- pipeline/test/update-prismic-snapshots.ts
+++ pipeline/test/update-prismic-snapshots.ts
@@ -1,14 +1,17 @@
+import { EOL } from "node:os";
 import fs from "node:fs/promises";
 import path from "node:path";
 import { createPrismicClient } from "../src/services/prismic";
+import { asTitle } from "../src/helpers";
+import { Writable } from "stream";
 
 const dataDir = path.resolve(__dirname, "prismic-snapshots");
 const documentIds = [
-  "YbjAThEAACEAcPYF",
-  "Y0U4GBEAAA__16h6",
-  "WcvPmSsAAG5B5-ox",
-  "XUGruhEAACYASyJh",
-  "XK9p2RIAAO1vQn__",
+  "YbjAThEAACEAcPYF", // articles	-	The enigma of the medieval folding almanac
+  "Y0U4GBEAAA__16h6", // articles	-	Tracing the roots of our fears and fixations
+  "WcvPmSsAAG5B5-ox", // articles	-	The Key to Memory: Follow your nose
+  "XUGruhEAACYASyJh", // webcomics	-	Footpath
+  "XK9p2RIAAO1vQn__", // webcomics	-	Groan
 ];
 
 const main = async () => {
@@ -26,7 +29,37 @@ const main = async () => {
       );
     })
   );
-  console.log("Done!");
+  console.log("Done saving snapshots.");
+
+  console.log("Adding comments to update script...");
+  const comments = new Map(
+    docs.map((doc) => [doc.id, `${doc.type}\t-\t${asTitle(doc.data.title)}`])
+  );
+  const tmpFile = `${__filename}.tmp`;
+  const thisScript = await fs.open(__filename, "r");
+  const newScript = await fs.open(tmpFile, "w");
+  const writeStream = newScript.createWriteStream();
+  const writeLine = lineWriter(writeStream);
+  for await (const line of thisScript.readLines()) {
+    const stringArrayItemLine =
+      /^(?<indent>\s*)"(?<id>.+)",(?<comment>\s*\/\/.*)?$/.exec(line);
+    if (stringArrayItemLine) {
+      const id = stringArrayItemLine.groups?.["id"];
+      const indent = stringArrayItemLine.groups?.["indent"];
+      const uncommentedLine = `${indent}"${id}",`;
+      if (id && comments.has(id)) {
+        await writeLine(uncommentedLine + `// ${comments.get(id)}`);
+        continue;
+      }
+    }
+    await writeLine(line);
+  }
+
+  await fs.rename(tmpFile, __filename);
+  console.log("Done.");
 };
+
+const lineWriter = (stream: Writable) => (line: string) =>
+  new Promise((resolve) => stream.write(line + EOL, "utf-8", resolve));
 
 main();
