b3d1e8140875acf61158c3eb4a723a77ea2e167b
jamie <j.parkinson@wellcome.ac.uk>
2023-05-02 14:29:37 +0100

    Decoupled, type safe, and tested query parameter validation

---
+++ api/src/controllers/validation.ts
@@ -0,0 +1,42 @@
+import { HttpError } from "./error";
+
+type StringLiteral<T> = T extends string
+  ? string extends T
+    ? never
+    : T
+  : never;
+
+type QueryValidatorConfig<Name, AllowedValue> = {
+  name: StringLiteral<Name>;
+  defaultValue: Readonly<StringLiteral<AllowedValue>>;
+  allowed: ReadonlyArray<StringLiteral<AllowedValue>>;
+};
+
+const isInArray = <T, A extends T>(
+  item: T,
+  array: ReadonlyArray<A>
+): item is A => array.includes(item as A);
+
+export const queryValidator =
+  <Name, AllowedValue>({
+    name,
+    defaultValue,
+    allowed,
+  }: QueryValidatorConfig<Name, AllowedValue>) =>
+  <Query extends { [key in typeof name]?: AllowedValue | string }>(
+    query: Query
+  ): AllowedValue => {
+    const value: AllowedValue | string | undefined = query[name];
+    if (typeof value === "undefined") {
+      return defaultValue;
+    } else if (isInArray(value, allowed)) {
+      return value;
+    } else {
+      throw new HttpError({
+        status: 400,
+        label:
+          `${name}: '${value}' is not a valid value. Please choose one of: ` +
+          allowed.map((s) => `'${s}'`).join(", "),
+      });
+    }
+  };
---
+++ api/test/validation.test.ts
@@ -0,0 +1,25 @@
+import { queryValidator } from "../src/controllers/validation";
+
+describe("query validator", () => {
+  const testValidator = queryValidator({
+    name: "test",
+    defaultValue: "a",
+    allowed: ["a", "b"],
+  });
+  it("extracts allowed values from query parameters", () => {
+    expect(testValidator({ test: "a" })).toBe("a");
+    expect(testValidator({ test: "b" })).toBe("b");
+  });
+
+  it("rejects values which are not in the allowlist", () => {
+    expect(() =>
+      testValidator({ test: "123" })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `"test: '123' is not a valid value. Please choose one of: 'a', 'b'"`
+    );
+  });
+
+  it("returns a given default value when the parameter is undefined", () => {
+    expect(testValidator({ test: undefined })).toBe("a");
+  });
+});
