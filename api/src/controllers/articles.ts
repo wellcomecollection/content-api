ae9ed3c87d4887e12f067a3b7b00ad517134bf22
jamie <j.parkinson@wellcome.ac.uk>
2023-05-03 16:53:17 +0100

    jest has a different view of how Promises work

--- api/src/controllers/articles.ts
+++ api/src/controllers/articles.ts
@@ -44,8 +44,8 @@ const articlesController = (
     const sort = sortValidator(queryParams);
     const sortOrder = sortOrderValidator(queryParams);
 
-    const searchResponse = await clients.elastic
-      .search<Displayable>({
+    try {
+      const searchResponse = await clients.elastic.search<Displayable>({
         index,
         _source: ["display"],
         query: queryString ? articlesQuery(queryString) : undefined,
@@ -58,53 +58,51 @@ const articlesController = (
           },
         ],
         ...paginationElasticBody(req.query),
-      })
-      .catch((error) => {
-        console.log(error.message.includes("too_many_nested_clauses"));
-        console.log(queryString?.length ?? 0);
-        if (
-          error instanceof elasticErrors.ResponseError &&
-          // This is an error we see from very long (spam) queries which contain
-          // many many terms and so overwhelm the multi_match query. The check
-          // for length is a heuristic so that if we get legitimate `too_many_nested_clauses`
-          // errors, we're still alerted to them
-          error.message.includes("too_many_nested_clauses") &&
-          encodeURIComponent(queryString || "").length > 2000
-        ) {
-          throw new HttpError({
-            status: 400,
-            label: "Bad Request",
-            description:
-              "Your query contained too many terms, please try again with a simpler query",
-          });
-        }
-        throw error;
       });
 
-    const results = searchResponse.hits.hits.flatMap((hit) =>
-      hit._source ? [hit._source.display] : []
-    );
+      const results = searchResponse.hits.hits.flatMap((hit) =>
+        hit._source ? [hit._source.display] : []
+      );
 
-    const requestUrl = new URL(
-      req.url,
-      `${req.protocol}://${req.headers.host}`
-    );
+      const requestUrl = new URL(
+        req.url,
+        `${req.protocol}://${req.headers.host}`
+      );
 
-    const totalResults =
-      typeof searchResponse.hits.total === "number"
-        ? searchResponse.hits.total
-        : searchResponse.hits.total?.value ?? 0;
+      const totalResults =
+        typeof searchResponse.hits.total === "number"
+          ? searchResponse.hits.total
+          : searchResponse.hits.total?.value ?? 0;
 
-    const paginationResponse = getPaginationResponse({
-      requestUrl,
-      totalResults,
-    });
+      const paginationResponse = getPaginationResponse({
+        requestUrl,
+        totalResults,
+      });
 
-    res.status(200).json({
-      type: "ResultList",
-      results,
-      ...paginationResponse,
-    });
+      res.status(200).json({
+        type: "ResultList",
+        results,
+        ...paginationResponse,
+      });
+    } catch (e) {
+      if (
+        e instanceof elasticErrors.ResponseError &&
+        // This is an error we see from very long (spam) queries which contain
+        // many many terms and so overwhelm the multi_match query. The check
+        // for length is a heuristic so that if we get legitimate `too_many_nested_clauses`
+        // errors, we're still alerted to them
+        e.message.includes("too_many_nested_clauses") &&
+        encodeURIComponent(queryString || "").length > 2000
+      ) {
+        throw new HttpError({
+          status: 400,
+          label: "Bad Request",
+          description:
+            "Your query contained too many terms, please try again with a simpler query",
+        });
+      }
+      throw e;
+    }
   });
 };
 
