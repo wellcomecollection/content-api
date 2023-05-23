23d4af8645df98da9fb0192e27ee476606af5ae2
jamie <j.parkinson@wellcome.ac.uk>
2023-05-23 12:17:41 +0100

    Another comment

--- api/src/helpers/requests.ts
+++ api/src/helpers/requests.ts
@@ -2,6 +2,9 @@ import { StringLiteral } from "../types";
 import { QueryDslQueryContainer } from "@elastic/elasticsearch/lib/api/types";
 import { ifDefined } from "./index";
 
+// Given an object of query parameters, and one with the same keys
+// of functions that create filter objects, apply all those functions
+// to the corresponding parameters and return an object of the filters
 export const pickFiltersFromQuery = <
   Name extends string,
   Query extends { [key in Name]?: string },
