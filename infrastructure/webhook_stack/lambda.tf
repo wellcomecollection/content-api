18cda59c9693763e46e4414968dd580ddc870b7c
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2023-07-18 16:20:36 +0000

    Apply auto-formatting rules

--- infrastructure/webhook_stack/lambda.tf
+++ infrastructure/webhook_stack/lambda.tf
@@ -5,27 +5,27 @@ locals {
 module "webhook_lambda" {
   source = "git@github.com:wellcomecollection/terraform-aws-lambda?ref=v1.2.0"
 
-  name = local.lambda_name
+  name    = local.lambda_name
   runtime = "nodejs18.x"
   handler = "lambda.handler"
 
-  filename = data.archive_file.empty_zip.output_path
+  filename    = data.archive_file.empty_zip.output_path
   memory_size = 128
-  timeout = 30
+  timeout     = 30
 
   error_alarm_topic_arn = var.lambda_alarm_topic_arn
 
   environment = {
     variables = {
-      SECRET_NAME = local.webhook_secret_name
+      SECRET_NAME    = local.webhook_secret_name
       EVENT_BUS_NAME = aws_cloudwatch_event_bus.document_unpublish_events.name
-      EVENT_TRIGGER = "document-unpublish"
+      EVENT_TRIGGER  = "document-unpublish"
     }
   }
 }
 
 resource "aws_lambda_function_url" "webhook" {
-  function_name = module.webhook_lambda.lambda.function_name
+  function_name      = module.webhook_lambda.lambda.function_name
   authorization_type = "NONE"
 }
 
@@ -40,36 +40,36 @@ data "archive_file" "empty_zip" {
 
 data "aws_iam_policy_document" "secrets_access" {
   statement {
-    effect = "Allow"
-    actions = ["secretsmanager:GetSecretValue"]
+    effect    = "Allow"
+    actions   = ["secretsmanager:GetSecretValue"]
     resources = module.secrets.arns
   }
 }
 
 data "aws_iam_policy_document" "events_publish" {
   statement {
-    effect = "Allow"
-    actions = ["events:PutEvents"]
+    effect    = "Allow"
+    actions   = ["events:PutEvents"]
     resources = [aws_cloudwatch_event_bus.document_unpublish_events.arn]
   }
 }
 
 resource "aws_iam_policy" "secrets_access" {
-  name = "${local.lambda_name}-secrets-access"
+  name   = "${local.lambda_name}-secrets-access"
   policy = data.aws_iam_policy_document.secrets_access.json
 }
 
 resource "aws_iam_policy" "events_publish" {
-  name = "${local.lambda_name}-events-publish"
+  name   = "${local.lambda_name}-events-publish"
   policy = data.aws_iam_policy_document.events_publish.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_secrets_access" {
-  role = module.webhook_lambda.lambda_role.name
+  role       = module.webhook_lambda.lambda_role.name
   policy_arn = aws_iam_policy.secrets_access.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_events_publish" {
-  role = module.webhook_lambda.lambda_role.name
+  role       = module.webhook_lambda.lambda_role.name
   policy_arn = aws_iam_policy.events_publish.arn
 }
