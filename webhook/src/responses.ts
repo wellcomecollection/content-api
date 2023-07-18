746b3924dee30cd843b1850c9b6a60ec0c12a0c1
jamie <j.parkinson@wellcome.org>
2023-07-18 17:39:10 +0100

    Log out responses

--- webhook/src/responses.ts
+++ webhook/src/responses.ts
@@ -1,4 +1,5 @@
 import { APIGatewayProxyResultV2 } from "aws-lambda/trigger/api-gateway-proxy";
+import log from "@weco/content-common/services/logging";
 
 export const response = ({
   status,
@@ -8,16 +9,20 @@ export const response = ({
   status: number;
   label: string;
   description?: string;
-}): APIGatewayProxyResultV2 => ({
-  statusCode: status,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    type: "Error",
-    errorType: "http",
-    httpStatus: status,
-    label,
-    description,
-  }),
-});
+}): APIGatewayProxyResultV2 => {
+  log.info("Response: ");
+  log.info(JSON.stringify({ status, label, description }), null, 2);
+  return {
+    statusCode: status,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      type: "Error",
+      errorType: "http",
+      httpStatus: status,
+      label,
+      description,
+    }),
+  };
+};
