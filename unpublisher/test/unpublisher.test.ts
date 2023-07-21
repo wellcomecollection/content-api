594954905839f3529424bc08eb634cdd126d7684
jamie <j.parkinson@wellcome.org>
2023-07-21 10:58:26 +0100

    Test for not found errors

--- unpublisher/test/unpublisher.test.ts
+++ unpublisher/test/unpublisher.test.ts
@@ -1,4 +1,7 @@
-import { Client as ElasticClient } from "@elastic/elasticsearch";
+import {
+  Client as ElasticClient,
+  errors as elasticErrors,
+} from "@elastic/elasticsearch";
 import { eventBridgePrismicEvent } from "../src/event";
 import { createHandler } from "../src/handler";
 import { Context } from "aws-lambda";
@@ -25,5 +28,24 @@ describe("content unpublisher", () => {
     expect(
       mockElasticClient.delete.mock.calls.map(([arg]) => arg.id)
     ).toIncludeAllMembers(documents);
+  });
+
+  it("does not error if a document is not found", async () => {
+    const event = eventBridgePrismicEvent(["test"]);
+    const mockElasticClient = {
+      delete: jest.fn().mockRejectedValue(
+        new elasticErrors.ResponseError({
+          meta: {} as any,
+          warnings: [],
+          statusCode: 404,
+        })
+      ),
+    };
+    const testHandler = createHandler(
+      { elastic: mockElasticClient as unknown as ElasticClient },
+      { index: "test" }
+    );
+
+    return expect(testHandler(event, {} as Context, () => {})).resolves;
   });
 });
