0330730101e97bbdf52c2a1ee0b90748b9ef922b
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2023-12-12 12:02:38 +0000

    Don't omit new properties, just pass everything in. Update tests

--- pipeline/src/types/prismic/images.ts
+++ pipeline/src/types/prismic/images.ts
@@ -5,18 +5,10 @@ type ImageDimensions = {
   height?: number;
 };
 
-// This type matches the one in the wc.org repo
-// https://github.com/prismicio/prismic-client/pull/326
-// These are new properties that are expected but not documented for yet or returned
-type CustomPrismicFilledImage = Omit<
-  prismic.FilledImageFieldImage,
-  "id" | "edit"
->;
-
 // Currently the Prismic types only allow you to specify 1 image
 type ThumbnailedImageField<Thumbnails extends Record<string, ImageDimensions>> =
-  CustomPrismicFilledImage & {
-    [Property in keyof Thumbnails]?: CustomPrismicFilledImage;
+  prismic.FilledImageFieldImage & {
+    [Property in keyof Thumbnails]?: prismic.FilledImageFieldImage;
   };
 
 export type PrismicImage = ThumbnailedImageField<{
