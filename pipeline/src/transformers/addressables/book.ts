5c48fc998aa69d6838baa9af51e1844ed5bb8bc0
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-11-25 11:55:23 +0000

    Use titleSubtitle for query title

--- pipeline/src/transformers/addressables/book.ts
+++ pipeline/src/transformers/addressables/book.ts
@@ -44,7 +44,7 @@ export const transformAddressableBook = (
     },
     query: {
       type: 'Book',
-      title,
+      title: titleSubtitle,
       description,
       body,
       contributors,
