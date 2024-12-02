412dd69e1b74b1cfba2964743d695f6f732b2221
davidpmccormick <d.mccormick@wellcome.ac.uk>
2024-12-02 20:24:13 +0000

    Don't include promo caption in query

--- pipeline/src/transformers/addressables/exhibitionHighlightTour.ts
+++ pipeline/src/transformers/addressables/exhibitionHighlightTour.ts
@@ -68,7 +68,7 @@ export const transformAddressableExhibitionHighlightTour = (
     },
     query: {
       type: 'Exhibition highlight tour' as const,
-      description,
+      description: introText,
     },
   };
 
