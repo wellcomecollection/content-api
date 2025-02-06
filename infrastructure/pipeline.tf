635a89d46312004baf3f5610ed7dfaa30b4fee14
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-02-06 11:42:16 +0000

    Create new pipeline

--- infrastructure/pipeline.tf
+++ infrastructure/pipeline.tf
@@ -10,3 +10,16 @@ module "pipeline_2025-01-20" {
 
   unpublish_event_rule = module.webhook.unpublish_event_rule
 }
+
+module "pipeline_2025-02-06" {
+  source = "./pipeline_stack"
+
+  pipeline_date           = "2025-02-06"
+  window_duration_minutes = 15
+  deployment_template_id  = "aws-storage-optimized"
+  logging_cluster_id      = local.logging_cluster_id
+  network_config          = local.network_config
+  lambda_alarm_topic_arn  = local.catalogue_lambda_alarn_topic_arn
+
+  unpublish_event_rule = module.webhook.unpublish_event_rule
+}
