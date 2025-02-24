ee6aeee6863814a71d284c6288bf4e4b97405182
Raphaelle Cantin <r.cantin@wellcome.ac.uk>
2025-02-24 13:24:14 +0000

    Remove pipeline 2025-02-06 from codebase

--- infrastructure/pipeline.tf
+++ infrastructure/pipeline.tf
@@ -1,15 +1,3 @@
-module "pipeline_2025-02-06" {
-  source                  = "./pipeline_stack"
-  pipeline_date           = "2025-02-06"
-  window_duration_minutes = 15
-  deployment_template_id  = "aws-storage-optimized"
-  logging_cluster_id      = local.logging_cluster_id
-  network_config          = local.network_config
-  lambda_alarm_topic_arn  = local.catalogue_lambda_alarn_topic_arn
-
-  unpublish_event_rule = module.webhook.unpublish_event_rule
-}
-
 module "pipeline_2025-02-17" {
   source                  = "./pipeline_stack"
   pipeline_date           = "2025-02-17"
