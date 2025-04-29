c9d6402212656619a6d9c795cbd77c0736a6f1d4
gestchild <g.estchild@wellcome.ac.uk>
2025-04-29 11:12:50 +0100

    add function for creating the exclusions filter

--- api/src/queries/events.ts
+++ api/src/queries/events.ts
@@ -95,6 +95,17 @@ export const eventsFilter = {
   }),
 };
 
+export const eventsExclusionsFilter = {
+  excludeFormats: (excludeFormats: string[]): TermsFilter => ({
+    values: excludeFormats,
+    esQuery: {
+      terms: {
+        'query.format.keyword': excludeFormats,
+      },
+    },
+  }),
+};
+
 export const eventsAggregations = {
   format: {
     terms: {
