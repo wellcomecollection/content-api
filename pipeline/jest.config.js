705e43395a700bed6306fbea79486530a18e4cfc
gestchild <g.estchild@wellcome.ac.uk>
2025-05-08 18:08:09 +0100

    don’t ignore the parse-duration module

--- pipeline/jest.config.js
+++ pipeline/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  transformIgnorePatterns: ['/node_modules/(?!parse-duration/)'],
 };
