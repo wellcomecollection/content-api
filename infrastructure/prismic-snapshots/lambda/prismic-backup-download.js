4e53a75621b3b4e8c0f415cf89df832a5cfbd13f
Agnes Garoux <a.garoux@wellcome.ac.uk>
2025-12-12 11:19:44 +0000

    event is one of elements in the items array

--- infrastructure/prismic-snapshots/lambda/prismic-backup-download.js
+++ infrastructure/prismic-snapshots/lambda/prismic-backup-download.js
@@ -1,5 +1,5 @@
 exports.handler = async (event, context) => {
   console.log('Prismic backup download Lambda started');
-  console.log('Assets to download', JSON.stringify(event.items, null, 2));
+  console.log('Assets to download', JSON.stringify(event, null, 2));
   return;
 };
